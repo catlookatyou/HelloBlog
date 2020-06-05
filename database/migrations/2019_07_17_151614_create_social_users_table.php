a615e30ea580c19db2a15ee7f33e63e381b64e31
catlookatyou <woozoo6834@gmail.com>
2020-06-05 16:18:19 +0800

    social_user_migration

--- database/migrations/2019_07_17_151614_create_social_users_table.php
+++ database/migrations/2019_07_17_151614_create_social_users_table.php
@@ -14,7 +14,7 @@ class CreateSocialUsersTable extends Migration
     public function up()
     {
         Schema::create('social_users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->integer('id');
             $table->string('provider_user_id')->primary();
             $table->string('provider');
             $table->integer('user_id')->unsigned();
